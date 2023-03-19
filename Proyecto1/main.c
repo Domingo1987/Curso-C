c6a86cc2c6e7d8c80768e3409db33546be4ad360
Domingo1987 <domingoperez1987@gmail.com>
2023-03-19 17:27:16 -0300

    Mensaje de commit

---
+++ Proyecto1/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hola, munhhhdo!");
+    
+    return 0;
+}
--- Proyecto1/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hola, munhhhdo!" << std::endl;
-    return 0;
-}
