05d9c462384a15104ce89db43dbd78b8c7ddc6cc
Domingo1987 <domingoperez1987@gmail.com>
2023-03-19 18:00:41 -0300

    commit link en doxy

--- Proyecto1/main.c
+++ Proyecto1/main.c
@@ -1,3 +1,11 @@
+/**
+ * @brief Ejemplo de primer Doxygen con enlace a link externo.
+ * @author Domingo Pérez
+ * @date 19/03/2023
+ * 
+ * Consulta el \link https://www.1001problemas.com Página de problemas de Lenguaje C \endlink .
+ */
+
 #include <stdio.h>
 
 int main() {
